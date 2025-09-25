211d68023f9afd1bea3cf9d8a10ec349344910f9
ankush-github-11 <ankush10yt@gmail.com>
2025-09-26 00:59:03 +0530

    Fixing the SkillsCard Hover Effect

--- components/ui/SkillsCard.tsx
+++ components/ui/SkillsCard.tsx
@@ -13,7 +13,8 @@ const SkillsCard: React.FC<SkillsCardProps> = ({ children, title }) => {
   return (
     <div
       className="
-        relative overflow-hidden 
+        skillsCardCSS
+        relative 
         flex flex-col items-center justify-center
         aspect-square w-full
         rounded-2xl shadow-md
@@ -22,22 +23,21 @@ const SkillsCard: React.FC<SkillsCardProps> = ({ children, title }) => {
     >
       {/* Glow Effects */}
       <div className="absolute bg-white h-2 w-50 blur-2xl" />
-      <div className="absolute top-17 bg-white h-3 w-25 blur-xl" />
-      <div className="absolute top-17 bg-white h-2 w-15 blur-lg" />
-      <div className="absolute top-17 bg-emerald h-2 w-15 blur-lg" />
+      <div className="absolute top-17 bg-white h-5 w-20 blur-xl" />
+      <div className="absolute top-17 bg-emerald h-2 w-12 blur-lg" />
 
       {/* Centered Logo */}
       <div className="relative flex items-center justify-center w-1/2 h-1/2">
         <svg
           viewBox="0 0 128 128"
-          className="text-textcolor w-full h-full [&_*]:fill-current [&_*]:stroke-current"
+          className="dark:text-cyan-50 text-cyan-950 w-full h-full [&_*]:fill-current [&_*]:stroke-current"
         >
           {children}
         </svg>
       </div>
 
       {/* Title at bottom */}
-      <div className="absolute bottom-2 text-textcolorless text-sm">
+      <div className="absolute bottom-[4px] dark:text-cyan-50 text-cyan-950 text-sm">
         {title}
       </div>
     </div>

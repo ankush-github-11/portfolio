cd16f7cebd9bb4b0a9286e6e63a00c85b3d9a091
ankush-github-11 <ankush10yt@gmail.com>
2025-12-27 22:51:03 +0530

    Fixing the Download Button

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -8,16 +8,14 @@ const page = () => {
         download
         className="
           fixed bottom-6 right-6 z-50
-          py-2.5 px-4
+          p-3
           rounded-full
           backdrop-blur-md
           bg-gradient-to-br from-cyan-500 to-emerald-500 
-          border border-bordercolor
           hover:bg-white/40 dark:hover:bg-black/40 flex gap-2 items-center
         "
       >
-        <span className="text-white font-bold text-md">Download</span>
-        <Download strokeWidth={3} size={20} className="text-white"/>
+        <Download strokeWidth={3} size={24} className="text-white"/>
       </a>
       <iframe
         src="/Ankush_Bhattacharjee_Resume.pdf"

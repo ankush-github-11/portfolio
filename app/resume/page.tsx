a863fc157754778dd85326b8ca73941e4e5868de
ankush-github-11 <ankush10yt@gmail.com>
2025-12-27 22:39:40 +0530

    Updating the Download Button

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,16 +7,17 @@ const page = () => {
         href="/Ankush_Bhattacharjee_Resume.pdf"
         download
         className="
-          fixed top-6 right-6 z-50
-          p-3
+          fixed bottom-6 right-6 z-50
+          py-2.5 px-4
           rounded-full
           backdrop-blur-md
-          bg-gradient-to-br from-cyan-500 to-emerald-500
+          bg-gradient-to-br from-cyan-500 to-emerald-500 
           border border-bordercolor
-          hover:bg-white/40 dark:hover:bg-black/40
+          hover:bg-white/40 dark:hover:bg-black/40 flex gap-2 items-center
         "
       >
-        <Download strokeWidth={3} className="text-white"/>
+        <span className="text-white font-bold text-md">Download</span>
+        <Download strokeWidth={3} size={20} className="text-white"/>
       </a>
       <iframe
         src="/Ankush_Bhattacharjee_Resume.pdf"

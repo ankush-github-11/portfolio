bf338ab3a61706057b8929afe0e82660a7892f1a
ankush-github-11 <ankush10yt@gmail.com>
2025-12-27 22:24:32 +0530

    Adding the Resume Download Button

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { Download } from "lucide-react";
 const page = () => {
   return (
-    <div className='h-screen w-[100%]'>
-        <iframe
-            src="/Ankush_Bhattacharjee_Resume.pdf"
-            className="w-full h-full" 
-            title="Ankush Bhattacharjee Resume"
-        />
+    <div className="relative h-screen w-full">
+      <a
+        href="/Ankush_Bhattacharjee_Resume.pdf"
+        download
+        className="
+          fixed top-6 right-6 z-50
+          p-3
+          rounded-full
+          backdrop-blur-md
+          bg-gradient-to-br from-cyan-500 to-emerald-500
+          border border-bordercolor
+          hover:bg-white/40 dark:hover:bg-black/40
+        "
+      >
+        <Download strokeWidth={3} className="text-white"/>
+      </a>
+      <iframe
+        src="/Ankush_Bhattacharjee_Resume.pdf"
+        className="w-full h-full"
+        title="Ankush Bhattacharjee Resume"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;

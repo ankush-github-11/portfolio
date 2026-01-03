0d04d2420d3dd7088d5155e2abc55a92da64fc17
ankush-github-11 <ankush10yt@gmail.com>
2026-01-03 23:56:20 +0530

    Fixing Contact Section

--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -51,7 +51,7 @@ const ContactForm: React.FC = () => {
   };
 
   return (
-    <section className="max-w-md mx-auto my-10 p-2 pt-3 md:pt-6 md:p-6 rounded-lg shadow-lg bg-bgcolorless">
+    <section className="w-full mx-auto my-10 p-2 pt-3 md:pt-6 md:p-6 rounded-lg shadow-lg bg-gray/10">
       <form onSubmit={handleSubmit} className="space-y-4">
         <div>
           <label className="block mb-1 font-medium text-gray-700 dark:text-gray-200">
@@ -62,7 +62,7 @@ const ContactForm: React.FC = () => {
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="w-full px-4 py-2 rounded-lg border-2 border-gray/50 dark:border-bordercolor outline-none focus:border-emerald-300 dark:focus:border-emerald-700"
+            className="w-full px-4 py-2 rounded-lg border-2 border-gray/50 dark:border-gray/35 outline-none focus:border-emerald-300 dark:focus:border-emerald-700"
             placeholder="Enter your name"
           />
         </div>
@@ -76,7 +76,7 @@ const ContactForm: React.FC = () => {
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="w-full px-4 py-2 rounded-lg border-2 border-gray/50 dark:border-bordercolor outline-none focus:border-emerald-300 dark:focus:border-emerald-700"
+            className="w-full px-4 py-2 rounded-lg border-2 border-gray/50 dark:border-gray/35 outline-none focus:border-emerald-300 dark:focus:border-emerald-700"
             placeholder="you@example.com"
           />
         </div>
@@ -90,7 +90,7 @@ const ContactForm: React.FC = () => {
             value={formData.message}
             onChange={handleChange}
             rows={4}
-            className="w-full px-4 py-2 rounded-lg border-2 border-gray/50 dark:border-bordercolor focus:border-emerald-300 dark:focus:border-emerald-700 outline-none"
+            className="w-full px-4 py-2 rounded-lg border-2 border-gray/50 dark:border-gray/35 focus:border-emerald-300 dark:focus:border-emerald-700 outline-none"
             placeholder="Write your message here..."
           />
         </div>
